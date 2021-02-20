9031145046d5d9486a7f34ae0664e67155925459
Roi Santos Ríos <roi.santos.rios@udc.es>
2021-02-20 11:11:06 +0100

    initial commit

---
+++ src/test/java/es/udc/fic/ri/mipractica/AppTest.java
@@ -0,0 +1,20 @@
+package es.udc.fic.ri.mipractica;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
