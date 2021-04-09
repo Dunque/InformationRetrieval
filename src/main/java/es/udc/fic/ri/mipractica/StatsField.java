48021f31e4c14f3238c83d4d3f8e35a1feaf344c
anton.concheiro <anton.concheiro@udc.es>
2021-04-09 12:54:49 +0200

    Hecho stats

--- src/main/java/es/udc/fic/ri/mipractica/StatsField.java
+++ src/main/java/es/udc/fic/ri/mipractica/StatsField.java
@@ -3,8 +3,11 @@ package es.udc.fic.ri.mipractica;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.CollectionStatistics;
 import org.apache.lucene.store.FSDirectory;
 
@@ -27,27 +30,44 @@ public class StatsField {
                 System.out.println(args[i] + args[i + 1]);
                 i++;
             } else if ("-field".equals(args[i])) {
-                field = args[i + 1];
-                System.out.println(args[i] + args[i + 1]);
-                i++;
+            	if(args.length>i+1 && !args[i+1].startsWith("-")) {
+            		field = args[i + 1];
+                    System.out.println(args[i] + args[i + 1]);
+                    i++;
+            	}else {
+            		System.out.println(usage);
+            		System.exit(-1);
+            	}
             }
         }
+    }
+    
+    static void printStatistics(IndexReader reader, String field) throws IOException{
+    	CollectionStatistics collectionStats = new CollectionStatistics(
+	            field,
+	            reader.maxDoc(),
+	            reader.getDocCount(field),
+	            reader.getSumTotalTermFreq(field),
+	            reader.getSumDocFreq(field)
+	            );
+		System.out.println(collectionStats.toString());
     }
 
     public static void main(String[] args) {
 
         parseArguments(args);
+        IndexReader reader=null;
         
         try {
-			IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
-			CollectionStatistics collectionStats = new CollectionStatistics(
-		            field.toString(),
-		            reader.maxDoc(),
-		            reader.getDocCount(field),
-		            reader.getSumTotalTermFreq(field),
-		            reader.getSumDocFreq(field)
-		            );
-			System.out.println(collectionStats.sumTotalTermFreq());
+			reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+			if(field==null) {
+				for(IndexableField ifield : reader.document(0).getFields()) 
+					printStatistics(reader,ifield.name());
+			}else {
+				printStatistics(reader,field);
+			}
+			reader.close();
+			
         } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
