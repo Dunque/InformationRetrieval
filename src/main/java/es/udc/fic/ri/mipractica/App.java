73e22b2c9c7f8ffc44ad118da5a21045e4c7df46
Dunque <roi.santos.rios@gmail.com>
2021-04-11 22:50:19 +0200

    refactorizado nombree

--- src/main/java/es/udc/fic/ri/mipractica/App.java
+++
@@ -1,8 +0,0 @@
-package es.udc.fic.ri.mipractica;
-
-public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/test/java/es/udc/fi/ri/mipractica/TermsClustersTest.java
@@ -0,0 +1,20 @@
+package es.udc.fi.ri.mipractica;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TermsClustersTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
