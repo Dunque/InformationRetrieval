19ecab9d05b9dde45d2b8e83e40845a76f65c072
Roi <roi.santos.rios@gmail.com>
2021-04-21 09:10:12 +0200

    bestterms y similarterms estan iguales y puestos en su sitio

--- README.md
+++ README.md
@@ -11,7 +11,10 @@ First build the project
 
 Then run the class you want with
 > java -jar ./target/(file).jar\
-> java -jar target/IndexFiles-0.0.1-SNAPSHOT-jar-with-dependencies.jar -onlyFiles
-> java -jar target/SimilarTerms-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+> java -jar target/IndexFiles-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 > java -jar target/WriteIndex-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
+> java -jar target/StatsField-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+> java -jar target/BestTerms-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
+> java -jar target/SimilarTerms-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+> java -jar target/TermsClusters-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
--- src/main/java/es/udc/fi/ri/mipractica/SimilarTerms.java
+++ src/main/java/es/udc/fi/ri/mipractica/SimilarTerms.java
@@ -1,22 +1,40 @@
 package es.udc.fi.ri.mipractica;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealVector;
-import org.apache.lucene.index.*;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.MultiTerms;
+import org.apache.lucene.index.PostingsEnum;
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.search.CollectionStatistics;
+import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.*;
-
 public class SimilarTerms {
 
-    static String indexPath = "index";
+    static String indexPath;
     static String field;
     static String term;
     static int top;
     static String rep;
+    static int k;
 
     static class Vector{
         private Map<Character,Integer> counter;
@@ -42,7 +60,7 @@ public class SimilarTerms {
             return this.length;
         }
 
-        double cosine_sim(TermsClusters.Vector v2) {
+        double cosine_sim(Vector v2) {
             Set<Character> commonchars= new HashSet<>(this.charSet);
             commonchars.retainAll(v2.getCharSet());
 
@@ -52,7 +70,6 @@ public class SimilarTerms {
             }
 
             double length = this.length * v2.getLength();
-            System.out.println("AAAAA"+length);
 
             double similarity = 0;
             if(length!=0) {
@@ -62,7 +79,7 @@ public class SimilarTerms {
         }
     }
 
-    private static TermsClusters.Vector word2vec(String word) {
+    private static Vector word2vec(String word) {
         Map<Character,Integer> counter = new HashMap<>();
         for(int i = 0;i<word.length();i++) {
             if(counter.get(word.charAt(i))!=null) {
@@ -83,7 +100,7 @@ public class SimilarTerms {
         }
         length = Math.sqrt(length);
 
-        return new TermsClusters.Vector(counter,charSet,length,word);
+        return new Vector(counter,charSet,length,word);
     }
 
     private static void parseArguments(String[] args) {
@@ -114,17 +131,20 @@ public class SimilarTerms {
                 i++;
             } else if ("-rep".equals(args[i])) {
                 rep = args[i + 1];
-                System.out.println(args[i] + args[i+1]);
+                System.out.println(args[i]);
+                i++;
+            } else if ("-k".equals(args[i])) {
+                k = Integer.parseInt(args[i + 1]);
+                System.out.println(args[i] + args[i + 1]);
                 i++;
             }
         }
     }
 
-
     public static void main(String[] args) {
 
         parseArguments(args);
-        TermsClusters.Vector v1 = word2vec(term);
+        Vector v1 = word2vec(term);
 
         IndexReader reader = null;
 
@@ -138,80 +158,62 @@ public class SimilarTerms {
 
         try {
             List<String> terms = new ArrayList<String>();
-            Map<String,Integer> frequencies = new HashMap<>();
-            for(int i = 0; i<reader.maxDoc();i++) {
+            Map<String,Double> frequencies = new HashMap<>();
+            for(int i = 0; i<reader.numDocs();i++) {
                 Terms vector = reader.getTermVector(i, field);
-                TermsEnum termsEnum = null;
-                termsEnum = vector.iterator();
-                BytesRef text = null;
-                while ((text = termsEnum.next()) != null) {
-                    String term = text.utf8ToString();
-                    TermsClusters.Vector v2 = word2vec(term);
-                    System.out.println(term);
-                    System.out.println(v1.cosine_sim(v2));
-
-                    //int freq = (int) termsEnum.totalTermFreq();
-                    //frequencies.put(term, freq);
-                    //System.out.println(term);
-                    //terms.add(term);
+                if(vector!=null) {
+                    TermsEnum termsEnum = null;
+                    termsEnum = vector.iterator();
+                    BytesRef text = null;
+                    while ((text = termsEnum.next()) != null) {
+                        String term = text.utf8ToString();
+                        Vector v2 = word2vec(term);
+                        frequencies.put(term,v1.cosine_sim(v2));
+                    }
                 }
             }
-            if(!terms.contains(term)) {
-                System.out.println("Term not found");
-            } else {
-                RealVector vector = new ArrayRealVector(terms.size());
-                int i = 0;
-                for(String termv : terms) {
-                    int value = frequencies.containsKey(termv) ? frequencies.get(termv) : 0;
-                    vector.setEntry(i++, value);
-                }
-                RealVector rv = (RealVector) vector.mapDivide(vector.getL1Norm());
-                RealVector vector2 = new ArrayRealVector(terms.size());
-                int j = 0;
-                for(String termv : terms) {
-                    int value = frequencies.get(term);
-                    vector2.setEntry(j++, value);
-                }
-                //int value = frequencies.containsKey(term) ? frequencies.get(term) : 0;
-                //vector2.setEntry(0, value);
-                RealVector rv2 = (RealVector) vector2.mapDivide(vector2.getL1Norm());
 
-                System.out.println(rv.dotProduct(rv2)/rv.getNorm()*rv2.getNorm());
-                System.out.println(rv.toString());
-                System.out.println(rv2.toString());
-                System.out.println(vector.toString());
-                System.out.println(vector2.toString());
-
-                RealVector vector3 = new ArrayRealVector(1);
-                vector3.setEntry(0, frequencies.get(term));
-                for(String termv : terms) {
-                    RealVector vector4 = new ArrayRealVector(1);
-                    vector4.setEntry(0, frequencies.get(termv));
-                    RealVector rv3 = (RealVector) vector3.mapDivide(vector3.getL1Norm());
-                    RealVector rv4 = (RealVector) vector4.mapDivide(vector4.getL1Norm());
-                    System.out.println(rv3.dotProduct(rv4)/rv3.getNorm()*rv4.getNorm());
-                    System.out.println(termv+" "+frequencies.get(termv)+rv4.toString());
+            List<Map.Entry<String, Double> > list =
+                    new LinkedList<Map.Entry<String, Double> >(frequencies.entrySet());
+            Collections.sort(list, new Comparator<Map.Entry<String, Double> >() {
+                public int compare(Map.Entry<String, Double> o1,
+                                   Map.Entry<String, Double> o2)
+                {
+                    return (o2.getValue()).compareTo(o1.getValue());
                 }
+            });
 
+            // put data from sorted list to hashmap
+            HashMap<String, Double> temp = new LinkedHashMap<String, Double>();
+            for (Map.Entry<String, Double> aa : list) {
+                temp.put(aa.getKey(), aa.getValue());
             }
+            int n = temp.size();
+            List<String> result = new ArrayList<>(n);
+            for(String s : temp.keySet()) {
+                result.add(s);
+            }
+            List<Double> result_sim = new ArrayList<>(n);
+            for(Double d : temp.values()) {
+                result_sim.add(d);
+            }
+
+            if(top>result.size()) {
+                top=result.size();
+            }else if(top<0) {
+                top=0;
+            }
+            System.out.println(top+ " terms more similar with "+ term);
+            for(int i = 0;i<top;i++) {
+                System.out.println("Term: "+result.get(i)+" Similarity: "+String.format("%.4f",result_sim.get(i))+"%");
+            }
+
+
+
         } catch (IOException e2) {
             // TODO Auto-generated catch block
             e2.printStackTrace();
         }
-
-        try {
-            if ((termPosting = MultiTerms.getTermPostingsEnum(reader, field, new BytesRef(term))) != null) {
-                int docId;
-
-                while ((docId = termPosting.nextDoc()) != PostingsEnum.NO_MORE_DOCS) {
-
-                }
-            }
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
         try {
             reader.close();
         } catch (IOException e) {
